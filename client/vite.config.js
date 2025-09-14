95881955572eb29f27c4ef136aaa076cc288bbae
Jeevanandham R <jeevarajasekarj35@gmail.com>
2025-09-15 03:37:52 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,8 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  root: 'client',
   plugins: [react()],
-  server: { port: 5173 },
-  preview: { port: 5173 },
-  base: "/Jeeva_Portfolio/"   // ✅ must have slashes
+  base: "/Jeevan_Portfolio/",  // GitHub repository name
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 })
+
